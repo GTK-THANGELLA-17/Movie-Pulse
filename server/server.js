f818c7121d75f843111066ff3b856ac0054548c2
G THANGELLA <161929651+GTK-THANGELLA-17@users.noreply.github.com>
2025-04-05 15:19:22 +0530

    Movie Pulse V1

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://movie-pulse-v1.vercel.app', 'http://localhost:5173', 'http://localhost:3000'],
+  origin: ['https://moviepulse-nu.vercel.app/', 'http://localhost:5173', 'http://localhost:3000'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization']
--- src/api/opinionsApi.ts
+++ src/api/opinionsApi.ts
@@ -1,7 +1,7 @@
 
 import { getBrowserFingerprint } from "@/lib/utils";
 
-const API_URL = 'https://moviepulse-api.onrender.com/api';
+const API_URL = 'https://moviepulse-api-snfl.onrender.com/';
 
 export interface OpinionData {
   category: 'film' | 'television' | 'youtube' | 'streaming';
